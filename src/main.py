fef306abbb43417a12f3ba46bf313c6b4ac96ade
Jonas <johnn-2006@hotmail.de>
2023-01-28 16:36:52 +0100

    Create setup with cx_freeze and add icon.

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import sys
 
+from PySide6.QtGui import QIcon
 from PySide6.QtWidgets import QApplication
 from PySide6.QtQml import QQmlApplicationEngine
 from PySide6.QtQml import qmlRegisterType
@@ -15,6 +16,8 @@ if __name__ == "__main__":
     Starts the QT GUI via PySide6
     """
     app = QApplication(sys.argv)
+    app.setWindowIcon(QIcon("src/View/AproxIcon.ico"))
+
     qmlRegisterType(CellDataAnalyzerViewModel, 'CellDataAnalyzerViewModel', 1, 0, 'CellDataAnalyzerViewModel')
     qmlRegisterType(OcvSocDataListViewModel, 'OcvSocDataListViewModel', 1, 0, 'OcvSocDataListViewModel')
     qmlRegisterType(OcvSocCellDataTable, 'OcvSocCellDataTable', 1, 0, 'OcvSocCellDataTable')
